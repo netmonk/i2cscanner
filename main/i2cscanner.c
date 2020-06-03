325a18ebcfd2e23ff7cc1dc94392fe5c4cac733e
netmonk <netmonk@netmonk.org>
2020-06-03 12:24:56 +0200

    adding some comments

--- main/i2cscanner.c
+++ main/i2cscanner.c
@@ -17,23 +17,24 @@
 #define NOP() asm volatile ("nop")
 
 
-static gpio_num_t i2c_gpio_sda = 21;
-static gpio_num_t i2c_gpio_scl = 22;
-static uint32_t i2c_frequency = 100000;
-static i2c_port_t i2c_port = I2C_NUM_0;
+static gpio_num_t i2c_gpio_sda = 21; /*!< SDA pin 21 CHANGE FOR YOUR OWN BOARD PINOUT*/
+static gpio_num_t i2c_gpio_scl = 22; /*!< SCL pin 22 CHANGE FOR YOUR OWN BOARD PINOUT*/
+static uint32_t i2c_frequency = 100000; /*!< defining bus freqency */
+static i2c_port_t i2c_port = I2C_NUM_0; /*!< declaring i2c_port as internal i2c_port_t structure to port 0*/
 
 
+/* setting up a software timer used in the main loop to wait 1 second */ 
 unsigned long IRAM_ATTR micros()
 {
-    return (unsigned long) (esp_timer_get_time());
+    return (unsigned long) (esp_timer_get_time());  /*!< get the current time in microsecond since startup */
 }
-void IRAM_ATTR delayMicroseconds(uint32_t us)
+void IRAM_ATTR delayMicroseconds(uint32_t us)  /*< 'us' is the time in microsecond to wait as argument */
 {
-    uint32_t m = micros();
-    if(us){
-        uint32_t e = (m + us);
-        if(m > e){ //overflow
-            while(micros() > e){
+    uint32_t m = micros(); /*< Get the time at begining of function */
+    if(us){               
+        uint32_t e = (m + us); /* e = time at entrance of the function + the time requested to wait */ 
+        if(m > e){ //overflow if m+us > uint32_t MAX
+            while(micros() > e){ 
                 NOP();
             }
         }
@@ -43,13 +44,7 @@ void IRAM_ATTR delayMicroseconds(uint32_t us)
     }
 }
 
-
-
-
-
-
-
-static esp_err_t i2c_master_driver_initialize(void)
+static esp_err_t i2c_master_driver_initialize(void)  /*< initialisation of i2c as a master on esp32 */
 {
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
@@ -64,9 +59,9 @@ static esp_err_t i2c_master_driver_initialize(void)
 
 
 
-static int do_i2cdetect_cmd(void)
+static int do_i2cdetect_cmd(void) i /*< main function which loops over i2c address space to initialise a session, send a byte and look for answer*/ 
 {
-    i2c_driver_install(i2c_port, I2C_MODE_MASTER, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+    i2c_driver_install(i2c_port, I2C_MODE_MASTER, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0); 
     i2c_master_driver_initialize();
     uint8_t address;
     printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\r\n");
@@ -81,12 +76,12 @@ static int do_i2cdetect_cmd(void)
             i2c_master_stop(cmd);
             esp_err_t ret = i2c_master_cmd_begin(i2c_port, cmd, 50 / portTICK_RATE_MS);
             i2c_cmd_link_delete(cmd);
-            if (ret == ESP_OK) {
-                printf("%02x ", address);
-            } else if (ret == ESP_ERR_TIMEOUT) {
+            if (ret == ESP_OK) { /* if device answer correctly print device address */
+                printf("%02x ", address); 
+            } else if (ret == ESP_ERR_TIMEOUT) { /* if device is detected but doesn't reply in time print UU*/ 
                 printf("UU ");
             } else {
-                printf("-- ");
+                printf("-- "); /* if no device is detected print --*/
             }
         }
         printf("\r\n");
@@ -99,12 +94,12 @@ static int do_i2cdetect_cmd(void)
 
 void app_main(void)
 { 
- i2c_master_driver_initialize();	
- while(1){
- do_i2cdetect_cmd(); 
- delayMicroseconds(100000);
- } 
+i2c_master_driver_initialize();	
 
+while(1){
+  do_i2cdetect_cmd(); 
+  delayMicroseconds(100000); /* wait 1 second between loops */ 
+  } 
 } 
 
 
